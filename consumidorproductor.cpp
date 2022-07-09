c6f4c38b45e44e7054bcdf1a60828ff91b99f6d8
Franksall <74364499+Franksall@users.noreply.github.com>
2022-07-08 23:29:17 -0500

    Se creo la clase que engloba consumidor y productor

---
+++ consumidorproductor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+// random
+#include <random>
+#include <ctime>
+// Para usar el sleep enWindows/Linux:
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif 
+
+
+class ConsumerProducer_Class{
+private:
+	std::string container[20]; // almacena
+	int container_counter = 0; // utilizado por el productor
+	int consumer_counter = 0; // usa consumido
+
+	// funcionamiento interno
+	bool advancing_semaphore = true; // true = procede
+	bool empty_semaphore = true;
+	bool full_semaphore = false;
+	void checkFull(); 
+	void checkEmpty(); 
+	// Bloqueo / PErmiso
+	void wait(); // bbloquear
+	void signal(); // permitir
+	// metodoso principales
+	void Producer(std::string item);
+	void Consumer();
+
+	// check
+	bool isAvailable(); // si
+public:
+	// Constructor/Destructor
+	ConsumerProducer_Class();
+	~ConsumerProducer_Class();
+
+	// Interfaz
+	void useProducer(std::string item); //productor y sconsu
+	void useConsumer();
+};
